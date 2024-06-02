295ebd52d5262d4276f45bf929584edf19629b37
Monisankar <mxn170301@darden.com>
2024-06-02 16:40:13 +0530

    chore: added cors, cookie parser and created express app and configs

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@
 
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is listening to PORT : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed: ", err);
+  });
 /*
 import express from "express";
 const app = express();
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,19 @@
+// using promise
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+// using async await
+// const asyncHandler = (requestHandler) => async (req, res, next) => {
+//   try {
+//     await requestHandler(req, res, next);
+//   } catch (error) {
+//     res.status(error.code || 500).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// };
+export { asyncHandler };
